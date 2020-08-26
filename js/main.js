ca9a0bd9b9e998d1fb5d893406b8a3cc3fa07bbe
Oleksandr Kharchenko <pan4enko48@gmail.com>
2020-08-26 20:47:44 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ getNameFilm.addEventListener('click', function() {
   let filmPart = document.getElementById('films').value;
   if (filmPart != 'none') {
     async function getAllCharacters() {
-      return await axios.get(`https://swapi.co/api/films/${filmPart}/`);
+      return await axios.get(`https://swapi.dev/api/films/${filmPart}/`);
     }
     getAllCharacters().then(characters =>
       characters.data.characters.forEach(character => {
